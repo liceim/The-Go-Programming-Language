9790fc31d71ef7518859df326f45c233e8585a18
Bin <libin.im@gmail.com>
2021-10-26 11:53:55 -0700

    Update pingpong.go

--- Chapter 9/ex9.5/pingpong.go
+++ Chapter 9/ex9.5/pingpong.go
@@ -1,3 +1,6 @@
+//Exercise 9.5: Write a program with two goroutines that send messages back and forth over two unbuffered channels in ping-pong fashion. How many communications per second can the program sustain?
+
+
 package main
 
 import (
